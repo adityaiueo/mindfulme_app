9fc3db9a063a55175b022ac111599e9988adaea1
adityaiueo <adityapriputra51@smp.belajar.id>
2024-02-29 21:54:52 +0700

    ngebuat getrunningapps()

--- android/app/src/main/kotlin/com/example/mindfulme_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mindfulme_app/MainActivity.kt
@@ -1,6 +1,33 @@
-package com.example.mindfulme_app
+package com.example.getrunningapps
 
+import android.app.ActivityManager
+import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "getRunningApps"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getRunningApps") {
+                val runningApps = getRunningApps()
+                result.success(runningApps)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getRunningApps(): ArrayList<String> {
+        val runningApps = ArrayList<String>()
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val runningAppProcesses = activityManager.runningAppProcesses
+        for (processInfo in runningAppProcesses) {
+            runningApps.add(processInfo.processName)
+        }
+        return runningApps
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:mindfulme_app/main_tabview/main_tabviewscreen.dart';
 import 'package:mindfulme_app/utils/colors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mindfulme_app/screen/intro_page.dart'; 
+import 'package:flutter/services.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,12 +19,10 @@ void main() async {
   );
 }
 
-
 class MyApp extends StatelessWidget {
-   final bool isIntroShown;
+  final bool isIntroShown;
 
   const MyApp({Key? key, required this.isIntroShown}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -30,30 +30,97 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Intro Screen',
       theme: ThemeData(
-          textTheme: const TextTheme(
-        displayLarge: TextStyle(
-          fontSize: 30,
-          color: MyColors.titleTextColor,
-          fontWeight: FontWeight.bold,
-        ),
-        displayMedium: TextStyle(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 30,
+            color: MyColors.titleTextColor,
+            fontWeight: FontWeight.bold,
+          ),
+          displayMedium: TextStyle(
             fontSize: 18,
             color: MyColors.subTitleTextColor,
             fontWeight: FontWeight.w400,
             wordSpacing: 1.2,
-            height: 1.2),
-        displaySmall: TextStyle(
-          fontSize: 18,
-          color: MyColors.titleTextColor,
-          fontWeight: FontWeight.bold,
+            height: 1.2,
+          ),
+          displaySmall: TextStyle(
+            fontSize: 18,
+            color: MyColors.titleTextColor,
+            fontWeight: FontWeight.bold,
+          ),
+          headlineMedium: TextStyle(
+            fontSize: 18,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        headlineMedium: TextStyle(
-          fontSize: 18,
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-      )),
+      ),
       home: isIntroShown ? const MainTabViewScreen() : const IntroPage(),
+    );
+  }
+}
+
+class GetRunningApp extends StatefulWidget {
+  const GetRunningApp({super.key});
+
+  @override
+  State<GetRunningApp> createState() => _GetRunningAppState();
+}
+
+class _GetRunningAppState extends State<GetRunningApp> {
+    // Membuat platform channel dengan nama 'getRunningApps'
+  static const platform = MethodChannel('getRunningApps');
+
+  // Menjadikan bidang runningApps late jika Anda ingin menginisialisasinya nanti
+  late List<String> runningApps;
+
+  @override
+  void initState() {
+    super.initState();
+    runningApps = [];
+  }
+
+  Future<void> getRunningApps() async {
+    try {
+      final List<dynamic> result =
+          await platform.invokeMethod('getRunningApps');
+      setState(() {
+        runningApps = result.cast<String>();
+      });
+    } on PlatformException catch (e) {
+      print(e.message);
+    }
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Get Running Apps Example'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: getRunningApps,
+                child: Text('Get Running Apps'),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: runningApps.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(runningApps[index]),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
